628dbcbfdd803246846a565e4943f274e587cec6
danil802010 <danil802010@gmail.com>
2024-03-31 16:47:43 +0300

    Update prod.js

--- prod.js
+++ prod.js
@@ -2,6 +2,7 @@ let prod;
 prod='<table><caption>Тоже какая-то таблица</caption>'
 +'<td id="lev">'
 +'</td><td id="prav">'
+  +'<a>А это наша правая колонка</a>'
 +'</td></table>'
 
 document.write(prod);
