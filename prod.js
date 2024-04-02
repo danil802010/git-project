36d6eb7200363959f4180eca5383757d38c550ad
danil802010 <danil802010@gmail.com>
2024-04-02 14:02:52 +0300

    prod.js

--- prod.js
+++ prod.js
@@ -1,6 +1,7 @@
 let prod;
 prod='<table><caption>Тоже какая-то таблица</caption>'
 +'<td id="lev">'
+  +'<a>Будем посмотреть</a><br>'
 +'</td><td id="prav">'
   +'<a>А это наша правая колонка</a>'
 +'</td></table>'
