2418af36d86f03db3b3b55270485bdff07a9e593
danil802010 <danil802010@gmail.com>
2024-04-04 16:49:53 +0300

    prod.js

--- prod.js
+++ prod.js
@@ -1,9 +1,15 @@
 let prod;
 prod='<table><caption>Тоже какая-то таблица</caption>'
 +'<td id="lev">'
-  +'<a>Поэкспериментируем снова</a><i> (пишем программу)</i><br>'
+ +'<a>Тут у нас писанина и будет</a><br>'
 +'</td><td id="prav">'
   +'<a>А это наша правая колонка</a>'
+   +'<a>Поэкспериментируем снова</a><i> (пишем программу)</i><br>'
+  +'Function.prototype.defer=function(ms){<br>'
+	+'  setTimeout(this, ms);}<br>'
+  +'function f(){<br>'
+	+'  alert("Privet!");}<br>'
++'  f.defer(2000); <i>//Privet!,2 sec</i><br>'
 +'</td></table>'
 
 document.write(prod);
