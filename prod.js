0f30f4b7a7db38a97ddfd6b7bddc2a299d66c911
danil802010 <danil802010@gmail.com>
2024-04-05 11:54:12 +0300

    prod.js

--- prod.js
+++ prod.js
@@ -37,6 +37,6 @@ prod='<table><caption>Тоже какая-то таблица</caption>'
   +'var cM=new CM(10000);<br>'
   +'cM.sWA(50);<br>'
   +'cM.run(); <i>//Кофе готов!</i><br>'
-+'</li></td></table>'
++'</td></table>'
 
 document.write(prod);
