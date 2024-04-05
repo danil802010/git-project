091a4a2b61e1a03a2d62ef2689121fed67d64b02
danil802010 <danil802010@gmail.com>
2024-04-05 08:37:18 +0300

    prod.js

--- prod.js
+++ prod.js
@@ -3,7 +3,7 @@ prod='<table><caption>Тоже какая-то таблица</caption>'
 +'<td id="lev">'
  +'<a>Тут у нас писанина и будет</a><br>'
 +'</td><td id="prav">'
-  +'<a>А это наша правая колонка</a>'
+  +'<a>А это наша правая колонка</a><br>'
    +'<a>Поэкспериментируем снова</a><i> (пишем программу)</i><br>'
   Function.prototype.defer=function(ms){
 	  var f=this;
