b9c4ca181b120fff58c0039a2e7db74cb1d2e31b
danil802010 <danil802010@gmail.com>
2024-04-10 16:38:16 +0300

    prom.js

--- prom.js
+++ prom.js
@@ -1,5 +1,7 @@
 let prom;
 prom='<table><td id="pravcool">'
+  +'<a>Пример использования программы:</a><br>'
+  +'let result=условие?значение1:значение2;<br>'
 +'</td><td id="lpcool">'
 +'</td><td id="levcool">'
 +'</td></table>'
