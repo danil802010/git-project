d0603c4c81e5f001180122081a84c71769584782
danil802010 <danil802010@gmail.com>
2024-04-14 16:37:40 +0300

    prom.js

--- prom.js
+++ prom.js
@@ -1,5 +1,5 @@
 let prom;
-prom='<table><td id="levcool">'
+prom='<table><td id="levcool">зырк'
   +'<a>Пример использования программы:</a><br>'
   +'<li>let result=условие ? значение1 : значение2;</li><br>'
 	 +'<ul><h4 style="margin-left:5%">Операторы нулевого слияния и присваивания: <a>??,??=</a></h4>'
